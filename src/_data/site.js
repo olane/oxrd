b3a072e2e6bcd923f07a14c76439c753751bf9f2
Oli Lane <622026+olane@users.noreply.github.com>
2026-09-22 20:10:24 +0100

    trim goatcounter config comment

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,3 @@
 module.exports = {
-  // GoatCounter site URL. Leave empty to disable analytics.
-  // To enable, register a site at https://www.goatcounter.com and set this to
-  // its URL, e.g. "https://orra.goatcounter.com" (or a custom domain).
   goatcounterUrl: "https://orra.goatcounter.com",
 };
